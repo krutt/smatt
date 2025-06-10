0c4503571e24ef631c5bbeaa0dc8330cc940a21a
Sitt Guruvanich <aekazitt@gmail.com>
2025-06-10 21:53:18 +0700

    feat: change root route to /smatt/

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,7 +17,7 @@ let menuOpen = ref(false)
     class="bg-white mt-0 px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"
   >
     <div class="container flex flex-wrap items-center justify-between mx-auto">
-      <a href="/" class="flex items-center">
+      <a href="/smatt/" class="flex items-center">
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
           >sMATT</span
         >
