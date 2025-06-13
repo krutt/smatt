8e3067e8d1569d7b4565134c17a1cf74997cb464
Sitt Guruvanich <aekazitt@gmail.com>
2025-06-13 18:48:44 +0700

    refactor: change RouterLink path to /smatt/

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -47,9 +47,9 @@ let menuOpen = ref(false)
         >
           <li>
             <RouterLink
-              to="/"
-              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
               aria-current="page"
+              class="inline-flex py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+              to="/smatt/"
             >
               Home
             </RouterLink>
