0e37f7c6ef9ac9f1d2d19b347023a47e533c0797
Sitt Guruvanich <aekazitt@gmail.com>
2025-06-13 15:46:59 +0700

    feat: style codeblock

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+/* ~~/tailwind.config.js */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  darkMode: ['selector', '[data-mode="dark"]'],
+  plugins: [
+    require('@tailwindcss/typography')
   ],
   theme: {
     extend: {
@@ -12,8 +18,4 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
-  darkMode: 'media',
 }
