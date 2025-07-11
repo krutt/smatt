4eae2371a57fb26358e7f30c1645eb39a25c8312
Sitt Guruvanich <aekazitt@gmail.com>
2025-07-11 14:48:29 +0700

    revert(styling): add shadcn/vue components
    This reverts commit 24952f6d99de87ded5d8dcb2074ab417f7999fe0.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <script lang="ts" setup>
 import { RouterView } from 'vue-router'
-import { onMounted } from 'vue'
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
-
-// Enable dark mode by default for Perplexity-like appearance
-onMounted(() => {
-  document.documentElement.classList.add('dark')
-})
 </script>
 <template>
-  <div class="min-h-screen bg-background text-foreground">
-    <Header />
-    <RouterView />
-    <Footer />
-    <Toaster />
-  </div>
+  <Header />
+  <RouterView />
+  <Footer />
+  <Toaster />
 </template>
