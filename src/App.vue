4e558650e73962e4785fb0617e3a7ef2a45c8c4c
Sitt Guruvanich <aekazitt@gmail.com>
2025-07-19 12:11:45 +0700

    feat: remove Footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script lang="ts" setup>
 import { RouterView } from 'vue-router'
 import Header from '@/components/Header.vue'
-import Footer from '@/components/Footer.vue'
 </script>
 <template>
   <Header />
   <RouterView />
-  <Footer />
   <Toaster />
 </template>
